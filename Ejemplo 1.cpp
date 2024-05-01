69728b358600edf2503990d624ce4f13869611d1
Dayanihdz <168614946+Dayanihdz@users.noreply.github.com>
2024-04-30 23:08:34 -0500

    Add files via upload

---
+++ Ejemplo 1.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+#include "string"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	//Mensaje de bienvenida
+	cout << "Hola! Este programa 1.8 imprime la matricula y el promedio de 5 notas de un alumno" << "\n";
+	
+	int matricula, a, b, c, d, e;
+	
+  	count<<"Introduzca el numero de matricula del alumno: ";
+  	cin>>matricula;
+    	
+ 	count<<"Introduzca la primera calificacion: ";
+ 	cin>>a;
+ 	
+ 	count<<"Introduzca la segunda calificacion: ";
+ 	cin>>b;
+	
+	count<<"Introduccion la tercera calificacion: ";
+	cin>>c;
+	
+	count<<"Introduzca la cuarta calificacion: ";
+	cin>>d;
+	
+	count<<"Introduzca la quinta calificacion: ";
+	cin>>e;
+	
+	count<<"La matricula del alumno es  " <<matricula<<end1;
+	
+	count<<"El promedio es: "<<((a+b+c+d+e)/5)<<end1;
